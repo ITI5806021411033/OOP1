4e6170bd16afc41d99e649499dc66083fe303fc9
DN <DN@DN-PC>
2016-02-11 00:16:00 +0700

    จาตุรนต์ จำนงค์พิพัฒน์ 5806021411033

---
+++ Seven_Dwarves/src/Seven_Dwarves.java
@@ -0,0 +1,136 @@
+import java.util.Scanner;
+
+public class Seven_Dwarves {
+
+	public static void main(String[] args) {
+		int midget01,midget02,midget03,midget04,midget05,midget06,midget07,midget08,midget09;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter midget01 : ");
+		midget01 = scan.nextInt();
+		System.out.print("Enter midget02 : ");
+		midget02 = scan.nextInt();
+		System.out.print("Enter midget03 : ");
+		midget03 = scan.nextInt();
+		System.out.print("Enter midget04 : ");
+		midget04 = scan.nextInt();
+		System.out.print("Enter midget05 : ");
+		midget05 = scan.nextInt();
+		System.out.print("Enter midget06 : ");
+		midget06 = scan.nextInt();
+		System.out.print("Enter midget07 : ");
+		midget07 = scan.nextInt();
+		System.out.print("Enter midget08 : ");
+		midget08 = scan.nextInt();
+		System.out.print("Enter midget09 : ");
+		midget09 = scan.nextInt();
+		System.out.println();
+		
+		if(midget03+midget04+midget05+midget06+midget07+midget08+midget09==100)
+			System.out.print("Fake Midget is midget01 & midget02");
+		
+		if(midget02+midget04+midget05+midget06+midget07+midget08+midget09==100)
+			System.out.print("Fake Midget is midget01 & midget03");
+		
+		if(midget03+midget02+midget05+midget06+midget07+midget08+midget09==100)
+			System.out.print("Fake Midget is midget01 & midget04");
+		
+		if(midget03+midget04+midget02+midget06+midget07+midget08+midget09==100)
+			System.out.print("Fake Midget is midget01 & midget05");
+		
+		if(midget03+midget04+midget05+midget02+midget07+midget08+midget09==100)
+			System.out.print("Fake Midget is midget01 & midget06");
+		
+		if(midget03+midget04+midget05+midget06+midget02+midget08+midget09==100)
+			System.out.print("Fake Midget is midget01 & midget07");
+		
+		if(midget03+midget04+midget05+midget06+midget07+midget02+midget09==100)
+			System.out.print("Fake Midget is midget01 & midget08");
+		
+		if(midget03+midget04+midget05+midget06+midget07+midget08+midget02==100)
+			System.out.print("Fake Midget is midget01 & midget09");
+		
+		if(midget01+midget04+midget05+midget06+midget07+midget08+midget09==100)
+			System.out.print("Fake Midget is midget02 & midget03");
+		
+		if(midget03+midget01+midget05+midget06+midget07+midget08+midget09==100)
+			System.out.print("Fake Midget is midget02 & midget04");
+		
+		if(midget03+midget04+midget01+midget06+midget07+midget08+midget09==100)
+			System.out.print("Fake Midget is midget02 & midget05");
+		
+		if(midget03+midget04+midget05+midget01+midget07+midget08+midget09==100)
+			System.out.print("Fake Midget is midget02 & midget06");
+		
+		if(midget03+midget04+midget05+midget06+midget01+midget08+midget09==100)
+			System.out.print("Fake Midget is midget02 & midget07");
+		
+		if(midget03+midget04+midget05+midget06+midget07+midget01+midget09==100)
+			System.out.print("Fake Midget is midget02 & midget08");
+		
+		if(midget03+midget04+midget05+midget06+midget07+midget08+midget01==100)
+			System.out.print("Fake Midget is midget02 & midget09");
+		
+		if(midget01+midget02+midget05+midget06+midget07+midget08+midget09==100)
+			System.out.print("Fake Midget is midget03 & midget04");
+		
+		if(midget01+midget04+midget02+midget06+midget07+midget08+midget09==100)
+			System.out.print("Fake Midget is midget03 & midget05");
+		
+		if(midget01+midget04+midget05+midget02+midget07+midget08+midget09==100)
+			System.out.print("Fake Midget is midget03 & midget06");
+		
+		if(midget01+midget04+midget05+midget06+midget02+midget08+midget09==100)
+			System.out.print("Fake Midget is midget03 & midget07");
+		
+		if(midget01+midget04+midget05+midget06+midget07+midget02+midget09==100)
+			System.out.print("Fake Midget is midget03 & midget08");
+		
+		if(midget01+midget04+midget05+midget06+midget07+midget08+midget02==100)
+			System.out.print("Fake Midget is midget03 & midget09");
+		
+		if(midget03+midget01+midget02+midget06+midget07+midget08+midget09==100)
+			System.out.print("Fake Midget is midget04 & midget05");
+		
+		if(midget03+midget01+midget05+midget02+midget07+midget08+midget09==100)
+			System.out.print("Fake Midget is midget04 & midget06");
+		
+		if(midget03+midget01+midget05+midget06+midget02+midget08+midget09==100)
+			System.out.print("Fake Midget is midget04 & midget07");
+		
+		if(midget03+midget01+midget05+midget06+midget07+midget02+midget09==100)
+			System.out.print("Fake Midget is midget04 & midget08");
+		
+		if(midget03+midget01+midget05+midget06+midget07+midget08+midget02==100)
+			System.out.print("Fake Midget is midget04 & midget09");
+		
+		if(midget03+midget04+midget01+midget02+midget07+midget08+midget09==100)
+			System.out.print("Fake Midget is midget05 & midget06");
+		
+		if(midget03+midget04+midget01+midget06+midget02+midget08+midget09==100)
+			System.out.print("Fake Midget is midget05 & midget07");
+		
+		if(midget03+midget04+midget01+midget06+midget07+midget02+midget09==100)
+			System.out.print("Fake Midget is midget05 & midget08");
+		
+		if(midget03+midget04+midget01+midget06+midget07+midget08+midget02==100)
+			System.out.print("Fake Midget is midget05 & midget09");
+		
+		if(midget03+midget04+midget05+midget01+midget02+midget08+midget09==100)
+			System.out.print("Fake Midget is midget06 & midget07");
+		
+		if(midget03+midget04+midget05+midget01+midget07+midget02+midget09==100)
+			System.out.print("Fake Midget is midget06 & midget08");
+		
+		if(midget03+midget04+midget05+midget01+midget07+midget08+midget02==100)
+			System.out.print("Fake Midget is midget06 & midget09");
+		
+		if(midget03+midget04+midget05+midget06+midget01+midget02+midget09==100)
+			System.out.print("Fake Midget is midget07 & midget08");
+		
+		if(midget03+midget04+midget05+midget06+midget01+midget08+midget02==100)
+			System.out.print("Fake Midget is midget07 & midget09");
+		
+		if(midget03+midget04+midget05+midget06+midget07+midget01+midget02==100)
+			System.out.print("Fake Midget is midget08 & midget09");
+	}
+}
